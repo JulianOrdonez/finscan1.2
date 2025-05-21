6382284e97e7813a8634e037910c31514a41c5f6
juliordonez229 <juliordonez229@gmail.com>
2025-05-21 18:52:23 +0000

    Nuevo

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,10 +1,10 @@
 class User {
-  int id;
+  int? id;
   String name;
   String email;
   String password;
 
-  User({required this.id, required this.name, required this.email, required this.password});
+  User({this.id, required this.name, required this.email, required this.password});
 
   Map<String, dynamic> toMap() {
     return {
