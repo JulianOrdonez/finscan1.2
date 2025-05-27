0ab376eb1e0842d10a4e5e4ced8b1c5d3aed7bfb
juliordonez229 <juliordonez229@gmail.com>
2025-05-27 14:47:17 +0000

    nuevo+

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -19,108 +19,108 @@ class SettingsScreen extends StatelessWidget {
     final TextEditingController supportController = TextEditingController();
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(height: 40), // Espacio superior
-              const Text(
-                'Ajustes',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueAccent,
-                ),
+ body: SingleChildScrollView(
+ child: Padding(
+ padding: const EdgeInsets.all(20.0),
+ child: Column(
+ crossAxisAlignment: CrossAxisAlignment.start,
+ children: <Widget>[
+ const SizedBox(height: 40), // Espacio superior
+ const Text(
+ 'Ajustes',
+ style: TextStyle(
+ fontSize: 32,
+ fontWeight: FontWeight.bold,
+ color: Colors.blueAccent,
+ ),
               ),
               const SizedBox(height: 30),
-              Container(
-                padding: const EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Icon(Icons.person, size: 30, color: Colors.blueAccent),
-                    SizedBox(width: 15),
-                    Expanded(
-                      child: Text(
-                        currentUser?.email ?? 'Usuario no disponible',
-                        style: TextStyle(fontSize: 18),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 30),
-              ListTile(
-                leading: Icon(Icons.monetization_on, color: Colors.blueAccent),
-                title: Text('Cambiar moneda', style: TextStyle(fontSize: 18)),
-                trailing: Icon(Icons.arrow_forward_ios),
-                onTap: () {
+ Container(
+ padding: const EdgeInsets.all(15.0),
+ decoration: BoxDecoration(
+ color: Colors.white,
+ borderRadius: BorderRadius.circular(10.0),
+ boxShadow: [
+ BoxShadow(
+ color: Colors.grey.withOpacity(0.2),
+ spreadRadius: 2,
+ blurRadius: 5,
+ offset: Offset(0, 3),
+ ),
+ ],
+ ),
+ child: Row(
+ children: [
+ Icon(Icons.person, size: 30, color: Colors.blueAccent),
+ SizedBox(width: 15),
+ Expanded(
+ child: Text(
+ currentUser?.email ?? 'Usuario no disponible',
+ style: TextStyle(fontSize: 18),
+ overflow: TextOverflow.ellipsis,
+ ),
+ ),
+ ],
+ ),
+ ),
+ const SizedBox(height: 30),
+ ListTile(
+ leading: Icon(Icons.monetization_on, color: Colors.blueAccent),
+ title: Text('Cambiar moneda', style: TextStyle(fontSize: 18)),
+ trailing: Icon(Icons.arrow_forward_ios),
+ onTap: () {
                   // TODO: Implement currency change functionality
-                },
-              ),
-              Divider(),
-              ListTile(
-                leading: Icon(Icons.brightness_6, color: Colors.blueAccent),
-                title: Text('Modo oscuro', style: TextStyle(fontSize: 18)),
-                trailing: Icon(Icons.arrow_forward_ios),
-                onTap: () {
+ },
+ ),
+ Divider(),
+ ListTile(
+ leading: Icon(Icons.brightness_6, color: Colors.blueAccent),
+ title: Text('Modo oscuro', style: TextStyle(fontSize: 18)),
+ trailing: Icon(Icons.arrow_forward_ios),
+ onTap: () {
                   // TODO: Implement dark mode toggle
-                },
-              ),
-              Divider(),
-              ListTile(
-                leading: Icon(Icons.insert_chart, color: Colors.blueAccent),
-                title: Text('Generar reporte', style: TextStyle(fontSize: 18)),
-                trailing: Icon(Icons.arrow_forward_ios),
-                onTap: () {
+ },
+ ),
+ Divider(),
+ ListTile(
+ leading: Icon(Icons.insert_chart, color: Colors.blueAccent),
+ title: Text('Generar reporte', style: TextStyle(fontSize: 18)),
+ trailing: Icon(Icons.arrow_forward_ios),
+ onTap: () {
                   // TODO: Implement report generation
-                },
-              ),
-              const SizedBox(height: 40),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    await authService.signOut();
-                    await Future.delayed(const Duration(milliseconds: 500));
-                    Navigator.pushNamedAndRemoveUntil(
-                      context,
-                      '/',
-                      (Route<dynamic> route) => false,
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent, // Color del botón de cerrar sesión
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                    textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                  ),
-                  child: const Text('Cerrar sesión'),
-                ),
-              ),
-              const SizedBox(height: 40),
-              const Text(
-                'Soporte',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueAccent,
-                ),
+ },
+ ),
+ const SizedBox(height: 40),
+ Center(
+ child: ElevatedButton(
+ onPressed: () async {
+ await authService.signOut();
+ await Future.delayed(const Duration(milliseconds: 500));
+ Navigator.pushNamedAndRemoveUntil(
+ context,
+ '/',
+                            (Route<dynamic> route) => false,
+ );
+ },
+ style: ElevatedButton.styleFrom(
+ backgroundColor: Colors.redAccent, // Color del botón de cerrar sesión
+ padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+ textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+ shape: RoundedRectangleBorder(
+ borderRadius: BorderRadius.circular(30.0),
+ ),
+ ),
+ child: const Text('Cerrar sesión'),
+ ),
+ ),
+ const SizedBox(height: 40),
+ const Text(
+ 'Soporte',
+ style: TextStyle(
+ fontSize: 24,
+ fontWeight: FontWeight.bold,
+ color: Colors.blueAccent,
+ ),
               ),
               const SizedBox(height: 15),
               TextField(
@@ -140,24 +140,23 @@ class SettingsScreen extends StatelessWidget {
               ),
               const SizedBox(height: 15),
               Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // TODO: Implement sending email functionality
-                    // You can use a package like 'url_launcher' to open the mail app
-                    // with the recipient and subject pre-filled.
-                    print('Support message: ${supportController.text}');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green, // Color del botón de enviar
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                    textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                  ),
-                  child: const Text('Enviar mensaje'),
-                ),
-              ],
+ child: ElevatedButton(
+ onPressed: () {
+                  // TODO: Implement sending email functionality
+                  // You can use a package like 'url_launcher' to open the mail app
+                  // with the recipient and subject pre-filled.
+ print('Support message: ${supportController.text}');
+ },
+ style: ElevatedButton.styleFrom(
+ backgroundColor: Colors.green, // Color del botón de enviar
+ padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+ textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+ shape: RoundedRectangleBorder(
+ borderRadius: BorderRadius.circular(30.0),
+ ),
+ ),
+ child: const Text('Enviar mensaje'),
+ ),
             ],
           ),
         ),
